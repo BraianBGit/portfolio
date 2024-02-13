c84a544536e471596316a648335f62a53aa1d73b
BraianBGit <braian.biale@ing.austral.edu.ar>
2024-02-13 17:31:56 -0300

    refactor data to json, small style changes

--- src/app/components/projectCard.tsx
+++ src/app/components/projectCard.tsx
@@ -5,7 +5,7 @@ const projectCard = ({imageSrc, imageAlt, name, description, technologies}: {
     imageAlt: string,
     name: string,
     description: string,
-    technologies: StaticImageData[]
+    technologies: string[]
 }) => {
     return <div
         className={"flex flex-row flex-grow flex-shrink sm:basis-1/2 md:basis-2/3 xl:basis-2/5 shadow-lg rounded-xl dark:bg-gray-900 hover:scale-105 transition-transform"}
@@ -16,9 +16,12 @@ const projectCard = ({imageSrc, imageAlt, name, description, technologies}: {
             <h3 className={"text-xl font-medium pb-2"}>{name}</h3>
             <p className={"py-4"}>{description}</p>
             <div className={"flex gap-2 mt-auto"}>
-                {technologies.map(technology => <Image src={technology} alt={imageAlt} width={30}
-                                                       height={30}
-                                                       className={"pt-2 hover:scale-110 transition-transform"}/>)}
+                {technologies.map(technology => <div
+                    className={"bg-cyan-800 rounded-xl p-2 hover:scale-110 transition-transform"}><Image src={technology}
+                                                                                                       alt={imageAlt}
+                                                                                                       width={30}
+                                                                                                       height={30} className={"max-w-fit h-8"}/>
+                </div>)}
             </div>
         </div>
     </div>
